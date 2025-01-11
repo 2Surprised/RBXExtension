c0478d293bda86fcc1a70c88ebb93d6b3e277825
2Surprised <2souprice@gmail.com>
2025-01-12 06:49:27 +0800

    Added ability to read friend activity/Cleaned up initial commit code

--- RBXExtension/friendActivityTracker.js
+++ RBXExtension/friendActivityTracker.js
@@ -1,119 +1,68 @@
-var gAttached = false;
-var gRequests = [];
-var gObjects = [];
-let attachedTab;
+let isDebuggerAlreadyAttached = false
+let attachedTabId = ''
 
-chrome.debugger.onEvent.addListener(function (source, method, params) {
-        if (method == "Network.requestWillBeSent") {
-            // If we see a url need to be handled, push it into index queue
-            var rUrl = params.request.url;
-            if (getTarget(rUrl) >= 0) {
-                gRequests.push(rUrl);
-            }
-        }
-        if (method == "Network.responseReceived") {
-            // We get its request id here, write it down to object queue
-            var eUrl = params.response.url;
-            var target = getTarget(eUrl);
-            if (target >= 0) {
-                gObjects.push({
-                    requestId: params.requestId,
-                    target: target,
-                    url: eUrl
-                });
-            }
-        }
-        if (method == "Network.loadingFinished" && gObjects.length > 0) {
-            // Pop out the request object from both object queue and request queue
-            var requestId = params.requestId;
-            var object = null;
-            for (var o in gObjects) {
-                if (requestId == gObjects[o].requestId) {
-                    object = gObjects.splice(o, 1)[0];
-                    break;
-                }
-            }
-            // Usually loadingFinished will be immediately after responseReceived
-            if (object == null) {
-                console.log('Failed!!');
-                return;
-            }
-            gRequests.splice(gRequests.indexOf(object.url), 1);
-            chrome.debugger.sendCommand(
-                source,
-                "Network.getResponseBody",
-                {"requestId": requestId},
-                function (response) {
-                    if (response) {
-                        console.log(response)
-                    } else {
-                        console.log("Empty response for " + object.url);
-                    }
-                    // If we don't have any request waiting for response, re-attach debugger
-                    // since without this step it will lead to memory leak.
-                    if (gRequests.length == 0) {
-                        chrome.debugger.detach({
-                            tabId: source.tabId
-                        }, function () {
-                            chrome.debugger.attach({
-                                tabId: source.tabId
-                            }, "1.0", function () {
-                                chrome.debugger.sendCommand({
-                                    tabId: source.tabId
-                                }, "Network.enable");
-                            });
-                        });
-                    }
-                });
-        }
+function attachDebugger(details) {
+    if (isDebuggerAlreadyAttached) return;
+    const { tabId } = details
+    if (!tabId > 0) return;
+
+    chrome.debugger.attach(
+        { tabId: tabId },
+        '1.3'
+    )
+    .then(
+        chrome.debugger.sendCommand(
+            { tabId: tabId },
+            'Network.enable'
+        )
+    )
+    isDebuggerAlreadyAttached = true
+    attachedTabId = tabId
+}
+
+// On startup, attach debugger
+// TODO: Use chrome.tabs.onCreated and onUpdated for more robust implementation
+chrome.webRequest.onBeforeRequest.addListener(attachDebugger, { urls: [ "https://*.roblox.com/*" ] });
+
+chrome.debugger.onDetach.addListener((source, reason) => {
+    isDebuggerAlreadyAttached = false
+    attachedTabId = ''
+})
+
+chrome.debugger.onEvent.addListener((source, method, params) => {
+    if (method === 'Network.responseReceived') {
+        const { requestId } = params
+
+        // TODO: Possible source of "No resource with given identifier found" errors
+        chrome.debugger.sendCommand(
+            source,
+            'Network.getResponseBody',
+            { requestId: requestId }
+        )
+        .then(result => {
+            const responseBody = result.body
+            isFriendActivity(responseBody)
+        })
     }
-);
+})
 
-var initialListener = function (details) {
-    console.log('is in initial listener now', gAttached)
-    if (gAttached) return;  // Only need once at the very first request, so block all following requests
-    var tabId = details.tabId;
-    if (tabId > 0) {
-        attachedTab = details.tabId
-        gAttached = true;
-        chrome.debugger.attach({
-            tabId: tabId
-        }, "1.0", function () {
-            chrome.debugger.sendCommand({
-                tabId: tabId
-            }, "Network.enable");
-        });
-        // Remove self since the debugger is attached already
-        chrome.webRequest.onBeforeRequest.removeListener(initialListener);
-    }
-};
-
-const isStillAttached = function(removedTabId, removeInfo) {
-    console.log('=== IS STILL ATTACHED? ===', attachedTab, removedTabId)
-    if (attachedTab === removedTabId) {
-        gAttached = false
-        console.log('=== NO ===')
-        chrome.webRequest.onBeforeRequest.addListener(initialListener, {urls: ["<all_urls>"]})
+function isFriendActivity(responseBody) {
+    console.log(responseBody)
+    try {
+        const object = JSON.parse(responseBody)
+        if (object.userPresences && userPresences.length <= 3) {
+            sendActivityAlert(object.userPresences)
+        }
+    } catch (error) {
+        if (!error.message === 'SyntaxError') { // If it's not invalid JSON
+            console.warn('isFriendActivity', error)
+        }
     }
 }
 
-// Attach debugger on startup
-chrome.webRequest.onBeforeRequest.addListener(initialListener, {urls: ["<all_urls>"]});
-// Attach debugger if tab is closed
-chrome.tabs.onRemoved.addListener(isStillAttached)
-
-// Filter if the url is what we want
-function getTarget(url) {
-    for (var i in TARGETS) {
-        var target = TARGETS[i];
-        if (url.match(target.url)) {
-            return i;
-        }
+function sendActivityAlert(userPresences) {
+    for (const userPresence of userPresences) {
+        // const { gameId, lastLocation, lastOnline, placeId, rootPlaceId, universeId, userId, userPresenceType } = userPresence
+        console.log(`=== USER PRESENCE ===`, userPresence)
     }
-    return -1;
 }
-
-const TARGETS = [
-    {url: 'roblox.com', desc: 'target1'},
-    {url: 'roblox.com/game'}
-]
