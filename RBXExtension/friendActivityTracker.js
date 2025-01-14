0a9d4ebae6130ec0f39db0b90615a52eaae77ce9
2Surprised <2souprice@gmail.com>
2025-01-15 02:34:33 +0800

    Improve efficiency with Promise.all implementation

--- RBXExtension/friendActivityTracker.js
+++ RBXExtension/friendActivityTracker.js
@@ -1,3 +1,4 @@
+// !! TODO: Prevent userhub websocket from disconnecting, or reopen connection when disconnected
 // TODO: Remove event listeners after use
 // TODO: Attempt switch to async await syntax
 // TODO: Add user settings option to disable activity tracker
@@ -6,63 +7,61 @@ let isDebuggerAlreadyAttached = false
 let attachedTabId = ''
 
 function attachDebugger(details) {
-    if (isDebuggerAlreadyAttached) return;
     const { tabId } = details
+    if (isDebuggerAlreadyAttached) return;
     if (!tabId > 0) return;
 
     chrome.debugger.attach(
         { tabId: tabId },
         '1.3'
     )
-    .then(
+    .then(() => {
         chrome.debugger.sendCommand(
             { tabId: tabId },
             'Network.enable'
         )
-    )
-    isDebuggerAlreadyAttached = true
-    attachedTabId = tabId
+        isDebuggerAlreadyAttached = true
+        attachedTabId = tabId
+    })
 }
 
 // On startup, attach debugger
 // TODO: Use chrome.tabs.onCreated and onUpdated for more robust implementation
 // TODO: Fix error with filter's inability to filter out chrome:// urls, despite specifying HTTPS protocol
 // https://stackoverflow.com/questions/24600495/chrome-tabs-executescript-cannot-access-a-chrome-url#comment130231485_24606853
-chrome.webRequest.onBeforeRequest.addListener(attachDebugger, { urls: [ "https://*.roblox.com/*" ] });
+chrome.webRequest.onBeforeRequest.addListener(attachDebugger, { urls: [ "https://*.roblox.com/*" ] })
 
-chrome.debugger.onDetach.addListener((source, reason) => {
+chrome.debugger.onDetach.addListener(() => {
     isDebuggerAlreadyAttached = false
     attachedTabId = ''
 })
 
 chrome.debugger.onEvent.addListener((source, method, params) => {
-    if (method === 'Network.responseReceived') {
-        const { requestId } = params
-        // TODO: Investigate source of "No resource with given identifier found" errors
-        // https://github.com/chromedp/chromedp/issues/1317#issuecomment-1561122839
-        // TODO: Investigate source of "No data found for resource with given identifier" errors
-        chrome.debugger.sendCommand(
-            source,
-            'Network.getResponseBody',
-            { requestId: requestId }
-        )
-        .then(result => {
-            const responseBody = result.body
-            isFriendActivity(responseBody)
-        })
-    }
+    if (!method === 'Network.responseReceived') return;
+    const { requestId } = params
+    // TODO: Investigate source of "No resource with given identifier found" errors
+    // https://github.com/chromedp/chromedp/issues/1317#issuecomment-1561122839
+    // TODO: Investigate source of "No data found for resource with given identifier" errors
+    chrome.debugger.sendCommand(
+        source,
+        'Network.getResponseBody',
+        { requestId: requestId }
+    )
+    .then(result => {
+        const responseBody = result.body
+        isFriendActivity(responseBody)
+    })
 })
 
 function isFriendActivity(responseBody) {
     try {
-        const object = JSON.parse(`${responseBody}`)
-        if (object.userPresences && object.userPresences.length <= 3) {
-            sendActivityAlert(object.userPresences)
-        }
     } catch (error) {
         if (!error.name === 'SyntaxError') {
             console.warn(error)
         }
+    }
+    if (object.userPresences && object.userPresences.length <= 3) {
+        sendActivityAlert(object.userPresences)
     }
 }
 
@@ -70,33 +69,36 @@ function isFriendActivity(responseBody) {
 // TODO: Prevent duplicate notifications from appearing for set duration
 // TODO: Implement filter for game activity with user-friendly interface
 // TODO: Add buttons to launch game client from notification
-function sendActivityAlert(userPresences) {
+async function sendActivityAlert(userPresences) {
     for (const userPresence of userPresences) {
-        const { gameId, lastLocation, lastOnline, placeId, rootPlaceId, universeId, userId, userPresenceType } = userPresence
+        const { placeId, rootPlaceId, userId, userPresenceType } = userPresence
+        const isSubPlace = false
         if (!rootPlaceId) return;
-        // 'Offline' = 0, 'Online' = 1, 'InGame' = 2, 'InStudio' = 3, 'Invisible' = 4
-        if (userPresenceType !== 2) return;
+        if (userPresenceType !== 2) return; // 'Offline' = 0, 'Online' = 1, 'InGame' = 2, 'InStudio' = 3, 'Invisible' = 4
+        if (placeId !== rootPlaceId) { isSubPlace = true }
 
         // TODO: Handle teleports to subplaces differently
-        fetch(`https://games.roblox.com/v1/games/multiget-place-details?placeIds=${rootPlaceId}`)
-        .then(response => response.json())
-        .then(result => (async function() {
-            const { placeId, name, description, sourceName, sourceDescription, url, builder, builderId, hasVerifiedBadge, isPlayable, reasonProhibited, universeId, universeRootPlaceId, price, imageToken } = result[0]
-            const userObject = await getUserFromUserId(userId)
-            const userDisplayName = userObject.displayName
-            const imageUrl = await getAvatarIconUrlFromUserId(userId)
-            const imageData = await getDataUrlFromWebResource(imageUrl)
+        const games = await fetch(`https://games.roblox.com/v1/games/multiget-place-details?placeIds=${rootPlaceId}`).then(response => response.json())
+        const { name } = games[0]
+        const userObject = getUserFromUserId(userId)
+        const imageUrl = getAvatarIconUrlFromUserId(userId)
+        const imageData = getDataUrlFromWebResource(imageUrl)
+        const userDisplayName = userObject.displayName
 
+        Promise.all([userObject, imageUrl, imageData])
+        .then(
             chrome.notifications.create({
                 iconUrl: imageData,
                 title: `${userDisplayName} is playing!`,
-                message: `Now in: ${name}!`,
+                message: `Now in: ${name}`,
                 contextMessage: '',
                 priority: 2,
                 type: 'basic',
                 silent: false
             })
-        })())
-        .catch(error => console.error(error))
+        )
+        .catch(error => {
+            console.error(error)
+        })
     }
 }
--- RBXExtension/utils/Utility.js
+++ RBXExtension/utils/Utility.js
@@ -4,7 +4,8 @@ export const RobloxWebsiteRegex = "^https?\:\/\/([a-z0-9\-]+\.)*roblox\.com(.+)?
 // Error messages
 export const errorMessage = {
     default: 'Something went wrong. Check the console for information.',
-    fileReaderFail: 'FileReader could not read the data provided.',
+    noErrorsPassed: 'No errors were passed to the error handler!',
+    fileReaderFail: 'FileReader could not process the data provided.',
     invalidUserId: 'Invalid user ID provided!',
     invalidWebResource: 'Invalid web resource provided!',
     avatarIconFail: 'Failed to fetch avatar icon.'
@@ -12,9 +13,9 @@ export const errorMessage = {
 
 // Functions
 export function errorHandler(errorObject) {
-    if (!errorObject) throw new Error('No errors were passed to the error handler!')
+    if (!errorObject) { throw new Error(errorMessage.noErrorsPassed) }
     const cause = errorObject.cause
-    if (cause) cause = `\nCause: ${cause}`
+    if (cause) { cause = `\nCause: ${cause}` }
     console.error(errorObject, cause)
     return errorMessage.default;
 }
@@ -29,41 +30,78 @@ export async function blobToBase64(blob) {
 }
 
 export async function getDataUrlFromWebResource(url) {
-    if (!url) throw new Error(errorMessage.invalidWebResource)
-    try {
+    if (!url) { throw new Error(errorMessage.invalidWebResource) }
+    return new Promise(async (resolve, reject) => {
         const blob = await fetch(url).then(response => response.blob())
         const objectUrl = await blobToBase64(blob)
-        return objectUrl;
-    } catch (error) {
-        return errorHandler(error)
-    }
+        resolve(objectUrl)
+    })
+    .catch(error => {
+        errorHandler(error)
+    })
 }
+
+// export async function getDataUrlFromWebResource(url) {
+//     if (!url) throw new Error(errorMessage.invalidWebResource);
+//     try {
+//         const blob = await fetch(url).then(response => response.blob())
+//         const objectUrl = await blobToBase64(blob)
+//         return objectUrl;
+//     } catch (error) {
+//         return errorHandler(error);
+//     }
+// }
 
 // Extension logic
 export function isUserIdValid(userId) {
-    if (!userId || typeof(userId) !== 'number') return false;
+    if (!userId || typeof(userId) !== 'number') { return false };
     return true;
 }
 
 export async function getUserFromUserId(userId) {
-    if (!isUserIdValid(userId)) throw new Error(errorMessage.invalidUserId);
-    try {
+    if (!isUserIdValid(userId)) { throw new Error(errorMessage.invalidUserId) }
+    return new Promise(async (resolve, reject) => {
         const userObject = await fetch(`https://users.roblox.com/v1/users/${userId}`).then(response => response.json())
-        return userObject;
-    } catch (error) {
-        return errorHandler(error);
-    }
+        resolve(userObject)
+    })
+    .catch(error => {
+        errorHandler(error)
+    })
 }
+
+// export async function getUserFromUserId(userId) {
+//     if (!isUserIdValid(userId)) throw new Error(errorMessage.invalidUserId);
+//     try {
+//         const userObject = await fetch(`https://users.roblox.com/v1/users/${userId}`).then(response => response.json())
+//         return userObject;
+//     } catch (error) {
+//         return errorHandler(error);
+//     }
+// }
 
 // type = 'avatar', 'avatar-bust', and 'avatar-headshot'
 export async function getAvatarIconUrlFromUserId(userId, type = 'avatar-headshot', size = 420) {
-    if (!isUserIdValid(userId)) throw new Error(errorMessage.invalidUserId);
-    try {
+    if (!isUserIdValid(userId)) { throw new Error(errorMessage.invalidUserId) }
+    return new Promise(async (resolve, reject) => {
         const response = await fetch(`https://thumbnails.roblox.com/v1/users/${type}?userIds=${userId}&size=${size}x${size}&format=Png&isCircular=false`).then(response => response.json())
         const iconObject = response.data[0]
-        if (iconObject.state !== 'Completed') throw new Error(errorMessage.avatarIconFail);
-        return iconObject.imageUrl;
-    } catch (error) {
-        return errorHandler(error);
-    }
+        if (iconObject.state !== 'Completed') { reject(errorMessage.avatarIconFail) }
+        resolve(iconObject.imageUrl)
+    })
+    .catch(error => {
+        errorHandler(error)
+    })
 }
+
+// // type = 'avatar', 'avatar-bust', and 'avatar-headshot'
+// export async function getAvatarIconUrlFromUserId(userId, type = 'avatar-headshot', size = 420) {
+//     if (!isUserIdValid(userId)) throw new Error(errorMessage.invalidUserId);
+//     try {
+//         const response = await fetch(`https://thumbnails.roblox.com/v1/users/${type}?userIds=${userId}&size=${size}x${size}&format=Png&isCircular=false`).then(response => response.json())
+//         const iconObject = response.data[0]
+//         if (iconObject.state !== 'Completed') throw new Error(errorMessage.avatarIconFail);
+//         return iconObject.imageUrl;
+//     } catch (error) {
+//         return errorHandler(error);
+//     }
+// }
