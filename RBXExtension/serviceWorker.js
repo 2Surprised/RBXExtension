e6e60a781692cda8681c7a73be0f3abb6f79cbfc
2Surprised <2souprice@gmail.com>
2025-01-23 00:34:44 +0800

    Add TypeScript and @types/chrome to codebase

--- RBXExtension/serviceWorker.js
+++ RBXExtension/serviceWorker.js
@@ -1,109 +1,85 @@
-// TODO: Implement notes system for friends
-// TODO: Completely overhaul the Roblox website chat
-// TODO: Save chat history of friends
-
-// TODO: Prevent authenticated user from appearing in notifications
-// TODO: Prevent false positives as a result of the Presence API returning invalid data.
-//       Check in with the Games API to check if a friend has truly left a game, as more
-//       often than not, the friend never left the game they were in.
-// TODO: Add user settings option to disable subplace tracker
-// TODO: Add user settings option to disable automated notification deletion
-import { getUserFromUserId, getAvatarIconUrlFromUserId, getDataUrlFromWebResource, RobloxWWWRegex, RobloxLoginRegex, RobloxPresenceRegex, removeValueFromArray } from './utils/utility.js'
-// Fun fact: Even though RobloxLoginRegexMatch prevents the debugger from attaching to roblox.com/login
-// tabs, the userhub websocket still works when the page is accessed through the Account Switcher feature.
-// This just avoids a lengthy implementation just to check if the user is logged in or not.
-const RobloxLoginRegexMatch = new RegExp(RobloxLoginRegex)
-const ALERT_TIMER_FOR_DETACHED_DEBUGGER = 2000
-const RETRY_TIMER_FOR_FAILED_REQUESTS = 5000
-const RESET_TIMER_FOR_RECENT_USER_PRESENCE = 15000
-const MAXIMUM_USER_PRESENCES_HANDLED_IN_SINGLE_REQUEST = 3
-let isTryingToAttach = false
-let isDebuggerAlreadyAttached = false
-let tabTitle = ''
-let attachedTabId = ''
-
-// TODO: Rewrite this horrible code
+import { getUserFromUserId, getAvatarIconUrlFromUserId, getDataUrlFromWebResource, RobloxWWWRegex, RobloxLoginRegex, RobloxPresenceRegex, removeValueFromArray } from './utils/utility.js';
+const RobloxLoginRegexMatch = new RegExp(RobloxLoginRegex);
+const ALERT_TIMER_FOR_DETACHED_DEBUGGER = 2000;
+const RETRY_TIMER_FOR_FAILED_REQUESTS = 5000;
+const RESET_TIMER_FOR_RECENT_USER_PRESENCE = 15000;
+const MAXIMUM_USER_PRESENCES_HANDLED_IN_SINGLE_REQUEST = 3;
+let isTryingToAttach = false;
+let isDebuggerAlreadyAttached = false;
+let tabTitle = '';
+let attachedTabId = '';
 async function attachDebugger(details) {
-    if (isTryingToAttach || isDebuggerAlreadyAttached) return;
-    isTryingToAttach = true
-    let tabId = 0
-    let tab = {}
-
+    if (isTryingToAttach || isDebuggerAlreadyAttached)
+        return;
+    isTryingToAttach = true;
+    let tabId = 0;
+    let tab = {};
     if (!Array.isArray(details)) {
-        // Only happens when attachDebugger is called by chrome.webRequest.onBeforeRequest
-        // chrome.webRequest.onBeforeRequest can trigger on tabs that aren't www.roblox.com
-        const tabInfo = await chrome.tabs.get(details.tabId)
+        const tabInfo = await chrome.tabs.get(details.tabId);
         if (!tabInfo.url.match(RobloxWWWRegex) || RobloxLoginRegexMatch.test(tabInfo.url)) {
-            isTryingToAttach = false
+            isTryingToAttach = false;
             return;
         }
-        ({ tabId } = details)
-        tab = tabInfo
-    } else {
-        // Only happens when attachDebugger is called by chrome.tabs.query()
+        ({ tabId } = details);
+        tab = tabInfo;
+    }
+    else {
         for (const tab of details) {
             if (tab.status !== 'unloaded' && !RobloxLoginRegexMatch.test(tab.url)) {
-                tabId = tab.id
+                tabId = tab.id;
                 break;
             }
         }
     }
-
     if (!tabId > 0) {
-        isTryingToAttach = false
+        isTryingToAttach = false;
         return;
     }
-
     try {
-        await chrome.debugger.attach({ tabId: tabId }, '1.3')
-        await chrome.debugger.sendCommand({ tabId: tabId }, 'Network.enable')
-    } catch (error) {
-        isTryingToAttach = false
-        // When a user updates a chrome:// tab by navigating from it, this error will occur.
-        // This error can be safely ignored, as the debugger is simply trying to attach to a
-        // still unloading chrome:// tab. After a few tries, it'll successfully attach to the
-        // newly loaded in tab.
-        if (error.message === 'Cannot access a chrome:// URL') return;
-        console.error(error)
+        await chrome.debugger.attach({ tabId: tabId }, '1.3');
+        await chrome.debugger.sendCommand({ tabId: tabId }, 'Network.enable');
     }
-
-    chrome.webRequest.onBeforeRequest.removeListener(attachDebugger)
-    isTryingToAttach = false
-    isDebuggerAlreadyAttached = true
-    attachedTabId = tabId
-    chrome.storage.session.set({ debuggerState: 'attached' })
-
-    if (JSON.stringify(tab) === '{}') { tab = await chrome.tabs.get(tabId) }
-    tabTitle = tab.title
-    console.log(`Currently attached to: ${tabTitle}`)
+    catch (error) {
+        isTryingToAttach = false;
+        if (error.message === 'Cannot access a chrome:// URL')
+            return;
+        console.error(error);
+    }
+    chrome.webRequest.onBeforeRequest.removeListener(attachDebugger);
+    isTryingToAttach = false;
+    isDebuggerAlreadyAttached = true;
+    attachedTabId = tabId;
+    chrome.storage.session.set({ debuggerState: 'attached' });
+    if (JSON.stringify(tab) === '{}') {
+        tab = await chrome.tabs.get(tabId);
+    }
+    tabTitle = tab.title;
+    console.log(`Currently attached to: ${tabTitle}`);
 }
-
 function attemptToAttachDebugger() {
-    chrome.tabs.query({ url: "https://www.roblox.com/*" }, attachDebugger)
-    chrome.webRequest.onBeforeRequest.addListener(attachDebugger, { urls: [ "https://www.roblox.com/*" ] })
+    chrome.tabs.query({ url: "https://www.roblox.com/*" }, attachDebugger);
+    chrome.webRequest.onBeforeRequest.addListener(attachDebugger, { urls: ["https://www.roblox.com/*"] });
 }
-
 function onDetach() {
-    isDebuggerAlreadyAttached = false
-    attachedTabId = ''
-    console.log('The debugger has been detached.')
-    attemptToAttachDebugger()
-    alertIfDebuggerIsDetached('warn')
+    isDebuggerAlreadyAttached = false;
+    attachedTabId = '';
+    console.log('The debugger has been detached.');
+    attemptToAttachDebugger();
+    alertIfDebuggerIsDetached('warn');
 }
-
 function onDetachWithoutReattach() {
-    isDebuggerAlreadyAttached = false
-    attachedTabId = ''
-    console.log('The debugger has been disabled.')
-    alertIfDebuggerIsDetached('suppress')
+    isDebuggerAlreadyAttached = false;
+    attachedTabId = '';
+    console.log('The debugger has been disabled.');
+    alertIfDebuggerIsDetached('suppress');
 }
-
-// action = 'warn' || 'suppress'
 function alertIfDebuggerIsDetached(action = 'warn') {
-    chrome.storage.session.set({ debuggerState: 'detached' })
-    if (action !== 'warn') return;
+    chrome.storage.session.set({ debuggerState: 'detached' });
+    if (action !== 'warn')
+        return;
     setTimeout(() => {
-        if (isDebuggerAlreadyAttached) return;
+        if (isDebuggerAlreadyAttached)
+            return;
         chrome.notifications.create({
             iconUrl: './utils/RBLX_Tilt_Primary_Black.png',
             title: 'Friend Activity Tracker is disabled!',
@@ -112,151 +88,117 @@ function alertIfDebuggerIsDetached(action = 'warn') {
             priority: 2,
             type: 'basic',
             silent: false
-        })
-    }, ALERT_TIMER_FOR_DETACHED_DEBUGGER)
+        });
+    }, ALERT_TIMER_FOR_DETACHED_DEBUGGER);
 }
-
-// On startup, attach debugger if feature is enabled
 (async function init() {
-    chrome.storage.session.set({ debuggerState: 'detached' })
-    const items = await chrome.storage.sync.get({ enableFriendActivityTracker: true })
+    chrome.storage.session.set({ debuggerState: 'detached' });
+    const items = await chrome.storage.sync.get({ enableFriendActivityTracker: true });
     if (items.enableFriendActivityTracker) {
-        attemptToAttachDebugger()
-        // On detach, attempt to attach again
-        chrome.debugger.onDetach.addListener(onDetach)
+        attemptToAttachDebugger();
+        chrome.debugger.onDetach.addListener(onDetach);
     }
-})()
-
-// When feature is enabled/disabled, attach/remove debugger
+})();
 chrome.storage.sync.onChanged.addListener(changes => {
-    if (!changes.enableFriendActivityTracker) return;
+    if (!changes.enableFriendActivityTracker)
+        return;
     if (changes.enableFriendActivityTracker.newValue === false) {
-        if (!attachedTabId) return;
-        chrome.debugger.detach({ tabId: attachedTabId }, onDetachWithoutReattach)
-    } else {
-        attemptToAttachDebugger()
+        if (!attachedTabId)
+            return;
+        chrome.debugger.detach({ tabId: attachedTabId }, onDetachWithoutReattach);
     }
-})
-
-// Detach debugger if no longer on www.roblox.com
+    else {
+        attemptToAttachDebugger();
+    }
+});
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tabId !== attachedTabId) return;
-    tabTitle = tab.title
-    if (!changeInfo.url) return;
+    if (tabId !== attachedTabId)
+        return;
+    tabTitle = tab.title;
+    if (!changeInfo.url)
+        return;
     if (!changeInfo.url.match(RobloxWWWRegex) || RobloxLoginRegexMatch.test(changeInfo.url)) {
-        // This does not trigger the onDetach event for the debugger, so the callback is necessary
-        chrome.debugger.detach({ tabId: attachedTabId }, onDetach)
+        chrome.debugger.detach({ tabId: attachedTabId }, onDetach);
     }
-})
-
+});
 chrome.debugger.onEvent.addListener(async (source, method, params) => {
-    const { requestId } = params
-
+    const { requestId } = params;
     if (method === 'Network.responseReceived' &&
         params.response.url.match(RobloxPresenceRegex) &&
-        params.response.status === 200
-    ) {
-        // https://github.com/chromedp/chromedp/issues/1317#issuecomment-1561122839
-        // These guard nodes prevent "No resource with given identifier",
-        // and "No data found for resource with given identifier" errors.
-        // Some are not required since the second and third conditionals above
-        // already remove them from the equation.
-        if (params.type === 'Preflight') return;
-        // "unknown" seems to be caused by local files being fetched, and any redirects to local files,
-        // which somehow causes the remote debugging protocol to be unable to retrieve their contents.
-        // Oddly enough, it seems like many other requests from the disk cache succeed, so the local
-        // files might be stored in long-term storage instead of in caches and RAM.
-        // if (params.response.securityState === 'unknown') return; // (Not required)
-        if (params.response.headers["content-length"] === 0) return;
-        // if (params.response.status === 204) return; // Ignore HTTP 204 No Content successes (Not required)
-        // The document will have no data when the response is first received, while fonts never have data
-        // if (params.type === 'Document' || params.type === 'Font') return; // (Not required)
-
+        params.response.status === 200) {
+        if (params.type === 'Preflight')
+            return;
+        if (params.response.headers["content-length"] === 0)
+            return;
         async function getResponseBodyAndPassOnData() {
-            const result = await chrome.debugger.sendCommand(
-                source,
-                'Network.getResponseBody',
-                { requestId: requestId }
-            )
-            const responseBody = result.body
-            isFriendActivity(responseBody)
+            const result = await chrome.debugger.sendCommand(source, 'Network.getResponseBody', { requestId: requestId });
+            const responseBody = result.body;
+            isFriendActivity(responseBody);
         }
-
         try {
-            await getResponseBodyAndPassOnData()
-        } catch (error) {
-            console.warn('Failed to retrieve response body, will retry in a few seconds.', requestId, params, error)
-            // This is a hacky solution, but if the original retrieval of the response body fails,
-            // this will simply wait a few seconds before attempting another retrieval.
+            await getResponseBodyAndPassOnData();
+        }
+        catch (error) {
+            console.warn('Failed to retrieve response body, will retry in a few seconds.', requestId, params, error);
             setTimeout(() => {
                 getResponseBodyAndPassOnData()
-                .catch(error => console.error('Failed to retrieve response body.', requestId, params, error))
-            }, RETRY_TIMER_FOR_FAILED_REQUESTS)
+                    .catch(error => console.error('Failed to retrieve response body.', requestId, params, error));
+            }, RETRY_TIMER_FOR_FAILED_REQUESTS);
         }
     }
-})
-
+});
 function isFriendActivity(responseBody) {
     try {
-        const object = JSON.parse(`${responseBody}`)
+        const object = JSON.parse(`${responseBody}`);
         if (object.userPresences && object.userPresences.length <= MAXIMUM_USER_PRESENCES_HANDLED_IN_SINGLE_REQUEST) {
-            sendActivityAlert(object.userPresences)
+            sendActivityAlert(object.userPresences);
         }
-    } catch (error) {
+    }
+    catch (error) {
         if (!error instanceof SyntaxError) {
-            console.warn(error)
+            console.warn(error);
         }
     }
 }
-
-// TODO: Implement session cache to prevent unnecessary strain on API
-// TODO: Implement filter for game activity with user-friendly interface
-// TODO: Add buttons to launch game client from notification
-const recentUserPresences = []
-
+const recentUserPresences = [];
 async function sendActivityAlert(userPresences) {
     for (const userPresence of userPresences) {
-        // Prevents duplicate notifications from coming through
-        if (recentUserPresences.includes(JSON.stringify(userPresence))) continue;
-        recentUserPresences.push(JSON.stringify(userPresence))
+        if (recentUserPresences.includes(JSON.stringify(userPresence)))
+            continue;
+        recentUserPresences.push(JSON.stringify(userPresence));
         setTimeout(() => {
-            removeValueFromArray(recentUserPresences, JSON.stringify(userPresence))
-        }, RESET_TIMER_FOR_RECENT_USER_PRESENCE)
-
-        const { placeId, rootPlaceId, userId, userPresenceType } = userPresence
-        if (!rootPlaceId) return;
-        if (userPresenceType !== 2) return; // 'Offline' = 0, 'Online' = 1, 'InGame' = 2, 'InStudio' = 3, 'Invisible' = 4
-        
-        let isSubPlace = false
-        let rootPlaceName = ''
-        let subPlaceName = ''
-        // TODO: Lead user to the game's page when clicking on notification
-        let placeUrl = ''
-        let userDisplayName = ''
-        let imageDataUrl = ''
-        // TODO: Create self-deleting notifications
-        let notificationId = ''
-        isSubPlace = placeId !== rootPlaceId
-
-        // TODO: Detect if a friend has joined another, then display the members of the party
+            removeValueFromArray(recentUserPresences, JSON.stringify(userPresence));
+        }, RESET_TIMER_FOR_RECENT_USER_PRESENCE);
+        const { placeId, rootPlaceId, userId, userPresenceType } = userPresence;
+        if (!rootPlaceId)
+            return;
+        if (userPresenceType !== 2)
+            return;
+        let isSubPlace = false;
+        let rootPlaceName = '';
+        let subPlaceName = '';
+        let placeUrl = '';
+        let userDisplayName = '';
+        let imageDataUrl = '';
+        let notificationId = '';
+        isSubPlace = placeId !== rootPlaceId;
         await (async function fetchData() {
-            const placeIdsToFetch = isSubPlace? `${rootPlaceId}&placeIds=${placeId}` : rootPlaceId
-            const games = await fetch(`https://games.roblox.com/v1/games/multiget-place-details?placeIds=${placeIdsToFetch}`).then(response => response.json())
-            rootPlaceName = games[0].name
-            placeUrl = games[0].url
+            const placeIdsToFetch = isSubPlace ? `${rootPlaceId}&placeIds=${placeId}` : rootPlaceId;
+            const games = await fetch(`https://games.roblox.com/v1/games/multiget-place-details?placeIds=${placeIdsToFetch}`).then(response => response.json());
+            rootPlaceName = games[0].name;
+            placeUrl = games[0].url;
             if (isSubPlace) {
-                subPlaceName = games[1].name
-                placeUrl = games[1].url
+                subPlaceName = games[1].name;
+                placeUrl = games[1].url;
             }
-            const userObjectPromise = getUserFromUserId(userId)
-            const imageUrlPromise = getAvatarIconUrlFromUserId(userId, 'avatar-headshot', 100)
-            const responses = await Promise.all([userObjectPromise, imageUrlPromise])
-            const userObject = responses[0]
-            const imageUrl = responses[1]
-            imageDataUrl = await getDataUrlFromWebResource(imageUrl)
-            userDisplayName = userObject.displayName
-        })()
-
+            const userObjectPromise = getUserFromUserId(userId);
+            const imageUrlPromise = getAvatarIconUrlFromUserId(userId, 'avatar-headshot', 100);
+            const responses = await Promise.all([userObjectPromise, imageUrlPromise]);
+            const userObject = responses[0];
+            const imageUrl = responses[1];
+            imageDataUrl = await getDataUrlFromWebResource(imageUrl);
+            userDisplayName = userObject.displayName;
+        })();
         notificationId = await chrome.notifications.create({
             iconUrl: imageDataUrl,
             title: !isSubPlace ? `${userDisplayName} is playing!` : `${userDisplayName} is in a subplace!`,
@@ -265,6 +207,6 @@ async function sendActivityAlert(userPresences) {
             priority: 2,
             type: 'basic',
             silent: false
-        })
+        });
     }
 }
--- RBXExtension/utils/utility.js
+++ RBXExtension/utils/utility.js
@@ -1,12 +1,8 @@
-// Regular expressions
-export const ExtensionName = 'RBXExtension'
-export const RobloxWebsiteRegex = "^https?\:\/\/([a-z0-9\-]+\.)*roblox\.com(.+)?$"
-export const RobloxWWWRegex = "^https?\:\/\/www\.roblox\.com"
-export const RobloxLoginRegex = "^https?\:\/\/www\.roblox\.com\/?([Ll]ogin)?$"
-export const RobloxPresenceRegex = "^https?\:\/\/presence\.roblox\.com"
-
-// TODO: Implement better error handling
-// Error messages
+export const ExtensionName = 'RBXExtension';
+export const RobloxWebsiteRegex = "^https?\:\/\/([a-z0-9\-]+\.)*roblox\.com(.+)?$";
+export const RobloxWWWRegex = "^https?\:\/\/www\.roblox\.com";
+export const RobloxLoginRegex = "^https?\:\/\/www\.roblox\.com\/?([Ll]ogin)?$";
+export const RobloxPresenceRegex = "^https?\:\/\/presence\.roblox\.com";
 export const errorMessage = {
     default: 'Something went wrong. Check the console for information.',
     noErrorsPassed: 'No errors were passed to the error handler!',
@@ -14,88 +10,93 @@ export const errorMessage = {
     invalidUserId: 'Invalid user ID provided!',
     invalidWebResource: 'Invalid web resource provided!',
     avatarIconFail: 'Failed to fetch avatar icon.'
-}
-
-// Functions
+};
 export function errorHandler(errorObject) {
-    if (!errorObject) { throw new Error(errorMessage.noErrorsPassed) }
-    const cause = errorObject.cause
-    if (cause) { cause = `\nCause: ${cause}` }
-    console.error(`${ExtensionName}:`, errorObject, cause)
+    if (!errorObject) {
+        throw new Error(errorMessage.noErrorsPassed);
+    }
+    const cause = errorObject.cause;
+    if (cause) {
+        cause = `\nCause: ${cause}`;
+    }
+    console.error(`${ExtensionName}:`, errorObject, cause);
     return errorMessage.default;
 }
-
 export function removeValueFromArray(array, value) {
-    if (typeof(value) === 'object') {
+    if (typeof (value) === 'object') {
         for (let index = 0; index < array.length; index++) {
-            const element = array[index]
+            const element = array[index];
             try {
                 if (JSON.stringify(element) === JSON.stringify(value)) {
-                    array.splice(index, 1)
+                    array.splice(index, 1);
                 }
-            } catch (error) {
-                // Some elements of the array may not be objects, this prevents an error from throwing
+            }
+            catch (error) {
             }
         }
-    } else {
+    }
+    else {
         for (let index = 0; index < array.length; index++) {
-            const element = array[index]
+            const element = array[index];
             if (element === value) {
-                array.splice(index, 1)
+                array.splice(index, 1);
             }
         }
     }
 }
-
 export async function blobToBase64(blob) {
     return new Promise((resolve, reject) => {
-        const fileReader = new FileReader()
-        fileReader.onload = () => resolve(fileReader.result)
-        fileReader.onerror = () => reject(errorMessage.fileReaderFail)
-        fileReader.readAsDataURL(blob)
-    })
+        const fileReader = new FileReader();
+        fileReader.onload = () => resolve(fileReader.result);
+        fileReader.onerror = () => reject(errorMessage.fileReaderFail);
+        fileReader.readAsDataURL(blob);
+    });
 }
-
 export async function getDataUrlFromWebResource(url) {
-    if (!url) { throw new Error(errorMessage.invalidWebResource) }
+    if (!url) {
+        throw new Error(errorMessage.invalidWebResource);
+    }
     return new Promise(async (resolve, _reject) => {
-        const blob = await fetch(url).then(response => response.blob())
-        const objectUrl = await blobToBase64(blob)
-        resolve(objectUrl)
+        const blob = await fetch(url).then(response => response.blob());
+        const objectUrl = await blobToBase64(blob);
+        resolve(objectUrl);
     })
-    .catch(error => {
-        errorHandler(error)
-    })
+        .catch(error => {
+        errorHandler(error);
+    });
 }
-
-// Extension logic
 export function isUserIdValid(userId) {
-    if (!userId || typeof(userId) !== 'number') { return false };
+    if (!userId || typeof (userId) !== 'number') {
+        return false;
+    }
+    ;
     return true;
 }
-
 export async function getUserFromUserId(userId) {
-    if (!isUserIdValid(userId)) { throw new Error(errorMessage.invalidUserId) }
+    if (!isUserIdValid(userId)) {
+        throw new Error(errorMessage.invalidUserId);
+    }
     return new Promise(async (resolve, _reject) => {
-        const userObject = await fetch(`https://users.roblox.com/v1/users/${userId}`).then(response => response.json())
-        resolve(userObject)
+        const userObject = await fetch(`https://users.roblox.com/v1/users/${userId}`).then(response => response.json());
+        resolve(userObject);
     })
-    .catch(error => {
-        errorHandler(error)
-    })
+        .catch(error => {
+        errorHandler(error);
+    });
 }
-
-// type = 'avatar', 'avatar-bust', and 'avatar-headshot'
 export async function getAvatarIconUrlFromUserId(userId, type = 'avatar-headshot', size = 420) {
-    if (!isUserIdValid(userId)) { throw new Error(errorMessage.invalidUserId) }
+    if (!isUserIdValid(userId)) {
+        throw new Error(errorMessage.invalidUserId);
+    }
     return new Promise(async (resolve, reject) => {
-        const response = await fetch(`https://thumbnails.roblox.com/v1/users/${type}?userIds=${userId}&size=${size}x${size}&format=Png&isCircular=false`).then(response => response.json())
-        const iconObject = response.data[0]
-        // TODO: Resolve issue with fetch request not returning a 200
-        if (iconObject.state !== 'Completed') { reject(errorMessage.avatarIconFail) }
-        resolve(iconObject.imageUrl)
+        const response = await fetch(`https://thumbnails.roblox.com/v1/users/${type}?userIds=${userId}&size=${size}x${size}&format=Png&isCircular=false`).then(response => response.json());
+        const iconObject = response.data[0];
+        if (iconObject.state !== 'Completed') {
+            reject(errorMessage.avatarIconFail);
+        }
+        resolve(iconObject.imageUrl);
     })
-    .catch(error => {
-        errorHandler(error)
-    })
+        .catch(error => {
+        errorHandler(error);
+    });
 }
